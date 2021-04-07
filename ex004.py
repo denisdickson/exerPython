49a41611e144f90d0acaea7e0a93658e9b4b7585
denisschirmer <denisdschirmer@gmail.com>
2021-04-07 16:23:54 -0300

    Dissecando variavel // sucessor antecessor

--- ex004.py
+++ ex004.py
@@ -1,8 +1,13 @@
-#Criar um programa que leia dois números e retorne a soma entre eles:
+#Dissecando uma váriavel : Fazer um programa que leia algo -
+# pelo teclado e mostre na tela o seu:
+#Tipo primitivo e todas informações possíveis
 
-def soma():
-    a = input('First number to sum: ')
-    b = input('Second number to sum: ')
-    return int(a)+int(b)
-
-print(soma())
+a = input('Digite algo: ')
+print(" o tipo primitivo desse valor é" ,  type(a))
+print('Só tem espaços?', a.isspace())
+print('is numeric?', a.isnumeric())
+print('é alfabetico?', a.isalpha())
+print('é alfanumeric?', a.isalnum())
+print('esta maisculo?', a.isupper())
+print('esta minusculo?', a.islower())
+print('esta capitalizada?', a.istitle())
---
+++ ex005.py
@@ -0,0 +1,6 @@
+# Faça um program que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+numero = int(input("Digite um número: "))
+#antecessor = numero - 1
+#sucessor = numero +1
+print("O valor digitado foi: {}, O antecessor é: {} E o sucessor: {}".format(
+    numero, numero-1, numero+1))
