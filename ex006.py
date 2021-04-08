838d4f52774fc915839a5dc13b64144788748ad7
denisschirmer <denisdschirmer@gmail.com>
2021-04-07 23:48:37 -0300

    none

--- ex006.py
+++ ex006.py
@@ -1,1 +1,14 @@
 #criar um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada 
+numero = int(input("Digite um número: "))
+
+print("O dobro de ", numero, " é ", numero * 2)
+print("O triplo de", numero, " é ", numero*3)
+print("A raiz quadrada de ", numero, " é ", numero**0.5)
+
+#n= int(input( 'digite o numero'))
+#d = n *2
+#t  = n*3
+#r= n ** 0.5
+#print('o dobro de {} vale {}'.format(n,(n*2))
+#print('o triplo de {} vale {}'.format(n(n*3)))
+#print('a raiz de {} vale {}'.format(n(n**0.5)))
---
+++ ex007.py
@@ -0,0 +1,11 @@
+#Calcule a média dos alunos 
+
+def media():
+    n1= float(input("Digite a primeira nota: "))
+    n2 = float(input("Digite a segunda nota: "))
+    return (n1 + n2) / 2
+
+print(media())
+
+#print('A media entre{} e {} é igual a {}'.format(n1,n2,media))
+#print('A media entre{:.1f} e {:.1f} é igual a {:.1f}'.format(n1,n2,media))
