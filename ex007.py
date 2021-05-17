1dac7b0d861bf2c7aa12c0ab26f3c78e73481fea
denisschirmer <denisdschirmer@gmail.com>
2021-05-17 16:12:20 -0300

    updated

--- ex007.py
+++ ex007.py
@@ -1,9 +1,10 @@
-#Calcule a média dos alunos 
+# Calcule a média dos alunos
 
 def media():
-    n1= float(input("Digite a primeira nota: "))
+    n1 = float(input("Digite a primeira nota: "))
     n2 = float(input("Digite a segunda nota: "))
     return (n1 + n2) / 2
+
 
 print(media())
 
--- ex008.py
+++ ex008.py
@@ -1,1 +1,19 @@
-#
+"""Desafio Conversor de Medidas
+
+Escrever programa que irá ler valor em metros e retornará em centimetros e milimetros
+
+Passos: 
+1 - Coletar número em metros
+2 - fazer conversão para centimetro
+3 - fazer conversão para milimetros
+4 - exibir conversão na tela
+
+"""
+
+medidaMetro = float(input("Digite uma medida em metros: "))
+
+medidaKm = medidaMetro * 100
+medidaMm = medidaMetro * 1000
+
+print("A medida de {} em centimetros é {}cm e em milimetros é {}mm".format(
+    medidaMetro,medidaKm, medidaMm))
